d09ff4c0f3b2fe93caad30cad57b17468616096f
Suvalian, Greg <Greg_Suvalian@McAfee.com>
2018-04-19 15:16:04 -0500

    asd

--- deploy.ps1
+++ deploy.ps1
@@ -2,12 +2,11 @@
 
 Param(
     [string] $ResourceGroupLocation = "southcentralus",
-    [string] $ResourceGroupName = 'Swarm-RG',
+    [string] $ResourceGroupName = 'Swarm-RG1',
     [string] $TemplateFile = 'azuredeploy.json',
-    [string] $TemplateParametersFile = 'azuredeploy.parameters-dev.json'
+    [string] $TemplateParametersFile = 'azuredeploy.parameters-show.json'
 )
-New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
+If (-not (Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue)) {New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose }
 New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
 -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile `
--TemplateParameterFile $TemplateParametersFile -Force -Verbose `
--ErrorVariable ErrorMessages -DeploymentDebugLogLevel All
+-TemplateParameterFile $TemplateParametersFile -Force -Verbose -ErrorVariable ErrorMessages -DeploymentDebugLogLevel All
